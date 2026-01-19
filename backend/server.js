3436b49c00a3ae713cbf13f89d41efdc80f8ec6f
arsalankhan8 <arsalankhan1102004@gmail.com>
2026-01-19 14:29:38 +0500

    server.js Updated

--- backend/server.js
+++ backend/server.js
@@ -101,9 +101,10 @@ app.use("/api", (req, res) => {
 
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+
 
 
 async function start() {
